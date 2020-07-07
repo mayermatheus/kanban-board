39ab2cb253554dd99e500a81f01356a807fe42e1
Matheus Mayer <matheusmayerduarte@gmail.com>
2020-07-07 02:11:47 -0300

    Commit Initial

---
+++ src/test/java/org/kanbanboard/kanbanboard/write/domain/application/CreateListCommandTest.java
@@ -0,0 +1,13 @@
+package org.kanbanboard.kanbanboard.write.domain.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CreateListCommandTest {
+
+    @Test
+    void dummy() {
+        assertThat(true).isTrue();
+    }
+}
