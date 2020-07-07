0424969cebea22861cf24a5db43de74db02c7216
Matheus Mayer <matheusmayerduarte@gmail.com>
2020-07-07 20:24:15 -0300

    Add testing first of command create list

--- src/test/java/org/kanbanboard/kanbanboard/write/domain/application/CreateListCommandTest.java
+++ src/test/java/org/kanbanboard/kanbanboard/write/domain/application/CreateListCommandTest.java
@@ -2,12 +2,25 @@ package org.kanbanboard.kanbanboard.write.domain.application;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CreateListCommandTest {
 
     @Test
-    void dummy() {
-        assertThat(true).isTrue();
+    void GivenValidDataMustRetrieveSameData() {
+        // GIVEN
+        UUID id = UUID.randomUUID();
+        int position = 1;
+        String name = "TODO";
+
+        // WHEN
+        CreateListCommand command = new CreateListCommand(id, position, name);
+
+        // THEN
+        assertThat(command.getId()).isEqualTo(id);
+        assertThat(command.getName()).isEqualTo(name);
+        assertThat(command.getPosition()).isEqualTo(position);
     }
 }
